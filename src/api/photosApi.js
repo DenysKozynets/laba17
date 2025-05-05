67aea6fcffdf6542a90cec3f2a184fe5665d91a5
Денис Козинец <kn23-d.kozynets@nubip.edu.ua>
2025-05-05 16:44:34 +0300

    photosApi.js

--- src/api/photosApi.js
+++ src/api/photosApi.js
@@ -2,4 +2,4 @@ export const fetchPhotos = async (page = 1, limit = 4) => {
   const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`);
   const data = await response.json();
   return data;
-};
+}; 
